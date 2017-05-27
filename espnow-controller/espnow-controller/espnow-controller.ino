608116d789a2b6297ba81a1ebfde6c7dbd609834
Nat <nat@cmmc.io>
2017-05-27 20:50:49 +0700

    Add checksum 2

--- espnow-controller/espnow-controller/espnow-controller.ino
+++ espnow-controller/espnow-controller/espnow-controller.ino
@@ -3,8 +3,8 @@
 #include <ESP8266WiFi.h>
 #include <Ticker.h>
 extern "C" {
-  #include <espnow.h>
-  #include <user_interface.h>
+#include <espnow.h>
+#include <user_interface.h>
 }
 #define rxPin 14
 #define txPin 12
@@ -14,19 +14,22 @@ SoftwareSerial swSerial(rxPin, txPin);
 #define WIFI_DEFAULT_CHANNEL 1
 #define DEBUG_SERIAL 1
 #if DEBUG_SERIAL
-    #define DEBUG_PRINTER Serial
-    #define DEBUG_PRINT(...) { DEBUG_PRINTER.print(__VA_ARGS__); }
-    #define DEBUG_PRINTLN(...) { DEBUG_PRINTER.println(__VA_ARGS__); }
-    #define DEBUG_PRINTF(...) { DEBUG_PRINTER.printf(__VA_ARGS__); }
+#define DEBUG_PRINTER Serial
+#define DEBUG_PRINT(...) { DEBUG_PRINTER.print(__VA_ARGS__); }
+#define DEBUG_PRINTLN(...) { DEBUG_PRINTER.println(__VA_ARGS__); }
+#define DEBUG_PRINTF(...) { DEBUG_PRINTER.printf(__VA_ARGS__); }
 #else
-    #define DEBUG_PRINT(...) { }
-    #define DEBUG_PRINTLN(...) { }
-    #define DEBUG_PRINTF(...) { }
+#define DEBUG_PRINT(...) { }
+#define DEBUG_PRINTLN(...) { }
+#define DEBUG_PRINTF(...) { }
 #endif
 
 bool ledState = LOW;
 Ticker ticker;
-uint8_t slave_mac[6];
+uint8_t self_ap_slave_macaddr[6];
+uint8_t self_sta_master_macaddr[6];
+
+byte buff[50];
 
 // SOFTAP_IF
 void printMacAddress(uint8_t* macaddr) {
@@ -42,9 +45,9 @@ void printMacAddress(uint8_t* macaddr) {
 void setup() {
   WiFi.disconnect();
   pinMode(LED_BUILTIN, OUTPUT);
-  #if DEBUG_SERIAL
-    Serial.begin(115200);
-  #endif
+#if DEBUG_SERIAL
+  Serial.begin(115200);
+#endif
 
   pinMode(rxPin, INPUT);
   pinMode(txPin, OUTPUT);
@@ -53,15 +56,18 @@ void setup() {
   DEBUG_PRINTLN("Initializing... Controller..");
   WiFi.mode(WIFI_STA);
   uint8_t macaddr[6];
+
   wifi_get_macaddr(STATION_IF, macaddr);
   DEBUG_PRINT("[master] address (STATION_IF): ");
+  memcpy(self_sta_master_macaddr, macaddr, 6);
   printMacAddress(macaddr);
 
   wifi_get_macaddr(SOFTAP_IF, macaddr);
   DEBUG_PRINT("[slave] address (SOFTAP_IF): ");
   printMacAddress(macaddr);
+  memcpy(self_ap_slave_macaddr, macaddr, 6);
 
-  if (esp_now_init()==0) {
+  if (esp_now_init() == 0) {
     DEBUG_PRINTLN("direct link  init ok");
   } else {
     DEBUG_PRINTLN("dl init failed");
@@ -69,26 +75,81 @@ void setup() {
     return;
   }
 
+  swSerial.println("ESPNOW SEND HELLO");
+  swSerial.write("HELLO", 5);
 
   esp_now_set_self_role(ESP_NOW_ROLE_CONTROLLER);
   esp_now_register_recv_cb([](uint8_t *macaddr, uint8_t *data, uint8_t len) {
     Serial.println("RECEIVE... ");
     for (size_t i = 0; i < len; i++) {
-      Serial.printf("BYTE[%d] - ", i);
-      Serial.println(data[i], HEX);
+      Serial.print(data[i], HEX);
     }
+    Serial.println();
     printMacAddress(macaddr);
     digitalWrite(LED_BUILTIN, ledState);
     ledState = !ledState;
 
+    uint8_t *client_slave_macaddr = macaddr;
 
-    int add_peer_status = esp_now_add_peer(macaddr, ESP_NOW_ROLE_SLAVE, WIFI_DEFAULT_CHANNEL, NULL, 0);
-    uint8_t* first_peer = esp_now_fetch_peer(true);
-    Serial.printf("ADD PEER STATUS: [%d] \r\n", add_peer_status);
+    byte sum = 0;
+    for (size_t i = 0; i < len; i++) {
+      if (i == 2 || i == 5 || i == 11 || i == 15 || i == 19 || i == 23 || i == 27) {
+        Serial.println();
+      }
 
-    uint8_t message[] = {1,1,1,1};
-    esp_now_send(first_peer, message, 4);
+      Serial.printf("%02x ", data[i]);
+      if (i == len - 1) {
+        Serial.println();
+        Serial.printf("calculated sum = %02x \r\n", sum);
+        Serial.printf(".....check sum = %02x \r\n", data[i]);
+      }
+      sum ^= data[i];
+    }
+
+    Serial.println();
+    uint32_t u32_temp;
+    uint32_t u32_humid;
+    uint32_t u32_batt;
+    u32_temp  = (data[14] << 24) | (data[13] << 16) | (data[12] << 8) | (data[11]);
+    u32_humid = (data[18] << 24) | (data[17] << 16) | (data[16] << 8) | (data[15]);
+    u32_batt  = (data[22] << 24) | (data[21] << 16) | (data[20] << 8) | (data[19]);
+
+    Serial.printf("temp: %lu \r\n", u32_temp);
+    Serial.printf("humid: %lu \r\n", u32_humid);
+    Serial.printf("batt: %lu \r\n", u32_batt);
+
+    Serial.printf("recv len = %d, macaddr len = %d \r\n", len, sizeof(macaddr));
+
+    //char mac[32] = {0};
+    //snprintf(mac, 32, "%02x:%02x:%02x:%02x:%02x:%02x", MAC2STR(macaddr));
+    // Serial.printf("mac: %s\r\n", mac);
+    // swSerial.write(mac, 32);
+    buff[0] = 0xFC;
+    buff[1] = 0xFD;
+    printMacAddress(self_sta_master_macaddr);
+    printMacAddress(client_slave_macaddr);
+    memcpy(buff+2, self_sta_master_macaddr, 6);
+    memcpy(buff+6+2, client_slave_macaddr, 6);
+    memcpy(buff+6+2+6, data, len);
+
+    // swSerial.write(0xFC);
+    // swSerial.write(0xFD);
+    // swSerial.write(self_sta_master_macaddr, 6);
+    swSerial.write(client_slave_macaddr, 6);
     swSerial.write(data, len);
+
+    byte sum2 = 0x00;
+    for (size_t i = 1; i <= len+2+6+6; i++) {
+      Serial.printf("%02x ", buff[i-1]);
+      if (i == 2 || i == 6+2 || i == 6+2+6) {
+        Serial.println();
+      }
+      sum2 ^= buff[i-1];
+    }
+
+    Serial.println();
+    Serial.printf("CHECK SUM 2 = %02x \r\n", sum2);
+    swSerial.write('\r');
   });
 
   esp_now_register_send_cb([](uint8_t* macaddr, uint8_t status) {
@@ -104,9 +165,8 @@ void setup() {
       DEBUG_PRINTLN("ESPNOW: SEND_FAILED");
       fail++;
     }
-    Serial.printf("[SUCCESS] = %lu/%lu \r\n", ok, ok+fail);
+    Serial.printf("[SUCCESS] = %lu/%lu \r\n", ok, ok + fail);
   });
-
   // DEBUG_PRINTF("ADD PEER: %d\r\n", add_peer_status);
 }
 
