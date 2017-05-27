6b7c7320fd6eeaed947d471cd967652020ae0524
Nat <nat@cmmc.io>
2017-05-28 06:34:30 +0700

    update cloud.

--- espnow-mqtt/dist/app.js
+++ espnow-mqtt/dist/app.js
@@ -10,9 +10,10 @@ exports.default = function (a, b) {
 
 var mqtt = require('mqtt');
 var client = mqtt.connect('mqtt://mqtt.cmmc.io');
+var moment = require('moment');
 
 var checksum = function checksum(message) {
-  var calculatedSum = void 0;
+  var calculatedSum = 0;
   var checkSum = message[message.length - 1];
   for (var i = 0; i < message.length - 1; i++) {
     var v = message[i];
@@ -24,7 +25,7 @@ var checksum = function checksum(message) {
 };
 
 client.on('connect', function () {
-  client.subscribe('/espnow/#');
+  client.subscribe('CMMC/espnow/#');
 });
 
 client.on('message', function (topic, message) {
@@ -38,33 +39,50 @@ client.on('message', function (topic, message) {
   console.log(message.length);
 
   if (checksum(message)) {
+    var mac1 = void 0,
+        mac2 = void 0;
     if (message[0] === 0xfc && message[1] === 0xfd) {
       console.log(message);
-      var mac1 = message.slice(2, 2 + 6);
-      var mac2 = message.slice(2 + 6, 2 + 6 + 6);
-      console.log(mac1, mac2);
+      mac1 = message.slice(2, 2 + 6);
+      mac2 = message.slice(2 + 6, 2 + 6 + 6);
       var len = message[2 + 6 + 6];
       var payload = message.slice(2 + 6 + 6 + 1, message.length - 1);
       console.log('len = ' + len + ', payload = ' + payload.toString('hex'));
       // if (checksum(payload)) {
       //   console.log('YAY!')
       // }
+      if (payload[0] === 0xff && payload[1] === 0xfa) {
+        var type = payload.slice(2, 5);
+        var name = payload.slice(5, 11);
+        var mac1String = mac1.toString('hex');
+        var mac2String = mac2.toString('hex');
+        var val1 = payload.readUInt32LE(11) || 0,
+            val2 = payload.readUInt32LE(15) || 0,
+            val3 = payload.readUInt32LE(19) || 0,
+            batt = payload.readUInt32LE(23) || 0;
+
+
+        console.log('type = ', type);
+        console.log('name = ', name.toString());
+        console.log('val1 = ', val1);
+        console.log('val2 = ', val2);
+        console.log('val3 = ', val3);
+        console.log('batt = ', batt);
+        console.log('[master] mac1 = ', mac1String);
+
+        console.log('[ slave] mac2 = ', mac2String);
+        client.publish('CMMC/espnow/' + mac1String + '/' + mac2String + '/val1', val1.toString());
+        client.publish('CMMC/espnow/' + mac1String + '/' + mac2String + '/val2', val2.toString());
+        client.publish('CMMC/espnow/' + mac1String + '/' + mac2String + '/batt', batt.toString());
+        client.publish('CMMC/espnow/' + mac1String + '/' + mac2String + '/val3', val3.toString());
+        client.publish('CMMC/espnow/' + mac1String + '/' + mac2String + '/nickname', name.toString());
+        client.publish('CMMC/espnow/' + mac1String + '/' + mac2String + '/sensor', type.toString('hex'));
+        client.publish('CMMC/espnow/' + mac1String + '/' + mac2String + '/updated', moment().unix().toString());
+        client.publish('CMMC/espnow/' + mac1String + '/' + mac2String + '/updatedText', moment().format('MMMM Do YYYY, h:mm:ss a'));
+      } else {
+        console.log('invalid header');
+      }
     }
-    //   if (message[0] === 0xff && message[1] === 0xfa) {
-    //     let type = message.slice(2, 5)
-    //     let name = message.slice(5, 11)
-    //     let mac = message.slice(27, 27 + 6)
-    //
-    //     console.log(`type = `, type)
-    //     console.log(`name = `, name.toString())
-    //     console.log(`val1 = `, message.readUInt32LE(11))
-    //     console.log(`val2 = `, message.readUInt32LE(15))
-    //     console.log(`val3 = `, message.readUInt32LE(19))
-    //     console.log(`batt = `, message.readUInt32LE(23))
-    //     console.log(`mac = `, mac.toString('hex'))
-    //   } else {
-    //     console.log('invalid header')
-    //   }
   } else {
     console.log('invalid checksum');
   }
--- espnow-mqtt/src/app.js
+++ espnow-mqtt/src/app.js
@@ -1,9 +1,10 @@
 export default (a, b) => a + b
 const mqtt = require('mqtt')
 const client = mqtt.connect('mqtt://mqtt.cmmc.io')
+const moment = require('moment')
 
 let checksum = (message) => {
-  let calculatedSum
+  let calculatedSum = 0
   let checkSum = message[message.length - 1]
   for (let i = 0; i < message.length - 1; i++) {
     let v = message[i]
@@ -15,7 +16,7 @@ let checksum = (message) => {
 }
 
 client.on('connect', function () {
-  client.subscribe('/espnow/#')
+  client.subscribe('CMMC/espnow/#')
 })
 
 client.on('message', function (topic, message) {
@@ -29,33 +30,50 @@ client.on('message', function (topic, message) {
   console.log(message.length)
 
   if (checksum(message)) {
+    let mac1, mac2
     if (message[0] === 0xfc && message[1] === 0xfd) {
       console.log(message)
-      let mac1 = message.slice(2, 2 + 6)
-      let mac2 = message.slice(2 + 6, 2 + 6 + 6)
-      console.log(mac1, mac2)
+      mac1 = message.slice(2, 2 + 6)
+      mac2 = message.slice(2 + 6, 2 + 6 + 6)
       let len = message[2 + 6 + 6]
       let payload = message.slice(2 + 6 + 6 + 1, message.length - 1)
       console.log(`len = ${len}, payload = ${payload.toString('hex')}`)
       // if (checksum(payload)) {
       //   console.log('YAY!')
       // }
+      if (payload[0] === 0xff && payload[1] === 0xfa) {
+        let type = payload.slice(2, 5)
+        let name = payload.slice(5, 11)
+        let mac1String = mac1.toString('hex')
+        let mac2String = mac2.toString('hex')
+        let [val1, val2, val3, batt] = [
+          payload.readUInt32LE(11) || 0,
+          payload.readUInt32LE(15) || 0,
+          payload.readUInt32LE(19) || 0,
+          payload.readUInt32LE(23) || 0
+        ]
+
+        console.log(`type = `, type)
+        console.log(`name = `, name.toString())
+        console.log(`val1 = `, val1)
+        console.log(`val2 = `, val2)
+        console.log(`val3 = `, val3)
+        console.log(`batt = `, batt)
+        console.log(`[master] mac1 = `, mac1String)
+
+        console.log(`[ slave] mac2 = `, mac2String)
+        client.publish(`CMMC/espnow/${mac1String}/${mac2String}/val1`, val1.toString())
+        client.publish(`CMMC/espnow/${mac1String}/${mac2String}/val2`, val2.toString())
+        client.publish(`CMMC/espnow/${mac1String}/${mac2String}/batt`, batt.toString())
+        client.publish(`CMMC/espnow/${mac1String}/${mac2String}/val3`, val3.toString())
+        client.publish(`CMMC/espnow/${mac1String}/${mac2String}/nickname`, name.toString())
+        client.publish(`CMMC/espnow/${mac1String}/${mac2String}/sensor`, type.toString('hex'))
+        client.publish(`CMMC/espnow/${mac1String}/${mac2String}/updated`, moment().unix().toString())
+        client.publish(`CMMC/espnow/${mac1String}/${mac2String}/updatedText`, moment().format('MMMM Do YYYY, h:mm:ss a'))
+      } else {
+        console.log('invalid header')
+      }
     }
-    //   if (message[0] === 0xff && message[1] === 0xfa) {
-    //     let type = message.slice(2, 5)
-    //     let name = message.slice(5, 11)
-    //     let mac = message.slice(27, 27 + 6)
-    //
-    //     console.log(`type = `, type)
-    //     console.log(`name = `, name.toString())
-    //     console.log(`val1 = `, message.readUInt32LE(11))
-    //     console.log(`val2 = `, message.readUInt32LE(15))
-    //     console.log(`val3 = `, message.readUInt32LE(19))
-    //     console.log(`batt = `, message.readUInt32LE(23))
-    //     console.log(`mac = `, mac.toString('hex'))
-    //   } else {
-    //     console.log('invalid header')
-    //   }
   } else {
     console.log('invalid checksum')
   }
