b7f04007ffb353d92675b2105ff20a353368ff52
Nat <nat@cmmc.io>
2017-07-07 07:49:03 +0700

    Add UInt32LEByte()

--- espnow-mqtt/tests/utils.spec.js
+++ espnow-mqtt/tests/utils.spec.js
@@ -1,5 +1,12 @@
 import * as Utils from '../src/utils'
 
+const hexFromChar = (c) => c.charCodeAt(0)
+const UInt32LEByte = (val) => {
+  const buff = Buffer.allocUnsafe(4)
+  buff.writeUInt32LE(val, 0)
+  return buff
+}
+
 describe('src/utils.js', () => {
   let validBuffer
   let invalidStartBytesBuffer
@@ -19,14 +26,13 @@ describe('src/utils.js', () => {
   console.log(`startBytes: `, startBytes)
   console.log(`endBytes: `, endBytes)
 
-  let hexFromChar = (c) => c.charCodeAt(0)
   beforeEach(() => {
     [type, field1, field2, field3, battery, nameLen, name] = [
       [0x00, 0x00, 0x01], /* type */
-      [0x64, 0x68, 0x31, 0x78], /* field1 */
-      [0x30, 0x32, 0xc4, 0x9], /* field2 */
-      [0x00, 0x00, 0xdc, 0x5], /* field3 */
-      [0x00, 0x00, 0x00, 0x10], /* battery */
+      UInt32LEByte(3200), /* field1 */
+      UInt32LEByte(7200), /* field2 */
+      UInt32LEByte(1111), /* field3 */
+      UInt32LEByte(8000), /* battery */
       [6], /* name len: 6 for 'nat001' */
       [hexFromChar('a'), hexFromChar('a'), hexFromChar('t'), 0x07, 0x08, 0x09] /* name */
     ]
@@ -80,30 +86,39 @@ describe('src/utils.js', () => {
     })
   })
 
-  describe('parsePayload', () => {
+  describe('payload parser', () => {
     it('should parse payload wrapper correctly', () => {
-      console.log(`validBuffer = `, validBuffer)
       const result = Utils.parsePayload(validBuffer)
-      console.log(`result = `, result)
       expect(result).toMatchObject({
-        mac1: Buffer.from([0x18, 0xfe, 0x34, 0xdb, 0x43, 0x10]),
-        mac2: Buffer.from([0x18, 0xfe, 0x34, 0xda, 0xf4, 0x98]),
+        mac1: Buffer.from(mac1),
+        mac2: Buffer.from(mac2),
         data: Buffer.from(dataBytes),
-        // data: Buffer.from([0xff, 0xfa, 0x00, 0x00, 0x01, 0x64, 0x68, 0x31, 0x78, 0x30, 0x32, 0xc4, 0x09, 0x00, 0x00,
-        //   0xdc, 0x05, 0x00, 0x00, 0x00, 0x10, 0x06, 0x61, 0x61, 0x74, 0x07, 0x08, 0x09, 0x33]),
-        len: 0x1d
-        // })
-        // console.log(`result = `, result)
+        len: dataBytes.length
       })
+      console.log(`validBuffer = `, validBuffer)
+      console.log(`result = `, result)
     })
 
-    describe('checksum function', () => {
-      it('should checksum correct', () => {
-        const data = Utils.slice(validBuffer, 0, validBuffer.length - 2)
-        // const result = Utils.checksum(data)
-        Utils.checksum(data)
-      })
+    it('should parse data payload correctly', () => {
+      const payload = Utils.parsePayload(validBuffer)
+      const dataPayload = payload.data
+      const result = Utils.parseDataPayload(dataPayload)
+      console.log(result)
+
+      // expect(result).toMatchObject({
+      //   mac1: Buffer.from(mac1),
+      //   mac2: Buffer.from(mac2),
+      //   data: Buffer.from(dataBytes),
+      //   len: dataBytes.length
+      // })
+    })
+  })
+
+  describe('checksum function', () => {
+    it('should checksum correct', () => {
+      const data = Utils.slice(validBuffer, 0, validBuffer.length - 2)
+      // const result = Utils.checksum(data)
+      Utils.checksum(data)
     })
   })
 })
-
