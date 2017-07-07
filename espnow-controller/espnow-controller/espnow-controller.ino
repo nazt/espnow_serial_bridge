f723b4046326df4681a0c215fc737f03f16abb23
Nat <nat@cmmc.io>
2017-07-07 13:43:43 +0700

    refactor / regroup

--- espnow-controller/espnow-controller/espnow-controller.ino
+++ espnow-controller/espnow-controller/espnow-controller.ino
@@ -92,45 +92,45 @@ void setup() {
 
 
   esp_now_set_self_role(ESP_NOW_ROLE_CONTROLLER);
-  esp_now_register_recv_cb([](uint8_t *macaddr, uint8_t *data, uint8_t len) {
-    Serial.println("RECEIVE... ");
-    for (size_t i = 0; i < len; i++) {
-      Serial.print(data[i], HEX);
-    }
-    Serial.println();
-    printMacAddress(macaddr);
-    digitalWrite(LED_BUILTIN, ledState);
+  esp_now_register_recv_cb([](uint8_t *client_mac_addr, uint8_t *data, uint8_t len) {
+    uint8_t *client_slave_macaddr = client_mac_addr;
     ledState = !ledState;
 
-    uint8_t *client_slave_macaddr = macaddr;
+    Serial.println("recv_cb... incoming mac: ");
+    printMacAddress(client_mac_addr);
+    // printMacAddress(self_sta_master_macaddr);
+    // printMacAddress(client_slave_macaddr);
+    digitalWrite(LED_BUILTIN, ledState);
+
+    for (size_t i = 0; i < len; i++) {
+      Serial.printf("%02x ", data[i]);
+      if (i%6==0 || i == len-1) {
+        Serial.println();
+      }
+    }
 
     byte sum = 0;
     for (size_t i = 0; i < len; i++) {
       if (i == 2 || i == 5 || i == 11 || i == 15 || i == 19 || i == 23 || i == 27) {
         Serial.println();
       }
-
       Serial.printf("%02x ", data[i]);
-      if (i == len - 1) {
-        Serial.println();
-        Serial.printf("calculated sum = %02x \r\n", sum);
-        Serial.printf(".....check sum = %02x \r\n", data[i]);
-      }
       sum ^= data[i];
     }
 
     Serial.println();
-    uint32_t u32_temp;
-    uint32_t u32_humid;
-    uint32_t u32_batt;
-
-    u32_temp  = (data[14] << 24) | (data[13] << 16) | (data[12] << 8) | (data[11]);
-    u32_humid = (data[18] << 24) | (data[17] << 16) | (data[16] << 8) | (data[15]);
-    u32_batt  = (data[22] << 24) | (data[21] << 16) | (data[20] << 8) | (data[19]);
-
-    Serial.printf("temp: %lu \r\n", u32_temp);
-    Serial.printf("humid: %lu \r\n", u32_humid);
-    Serial.printf("batt: %lu \r\n", u32_batt);
+    Serial.printf("calculated sum = %02x \r\n", sum);
+    Serial.printf(".....check sum = %02x \r\n", data[len-1]);
+    // uint32_t u32_temp;
+    // uint32_t u32_humid;
+    // uint32_t u32_batt;
+    // u32_temp  = (data[14] << 24) | (data[13] << 16) | (data[12] << 8) | (data[11]);
+    // u32_humid = (data[18] << 24) | (data[17] << 16) | (data[16] << 8) | (data[15]);
+    // u32_batt  = (data[22] << 24) | (data[21] << 16) | (data[20] << 8) | (data[19]);
+    //
+    // Serial.printf("temp: %lu \r\n", u32_temp);
+    // Serial.printf("humid: %lu \r\n", u32_humid);
+    // Serial.printf("batt: %lu \r\n", u32_batt);
 
     Serial.printf("recv len = %d, macaddr len = %d \r\n", len, sizeof(macaddr));
 
@@ -139,16 +139,14 @@ void setup() {
     // Serial.printf("mac: %s\r\n", mac);
     // swSerial.write(mac, 32);
 
-    buff[0] = 0xFC;
-    buff[1] = 0xFD;
-    printMacAddress(self_sta_master_macaddr);
-    printMacAddress(client_slave_macaddr);
+    buff[0] = 0xfc;
+    buff[1] = 0xfd;
 
     memcpy(buff+2, self_sta_master_macaddr, 6);
     memcpy(buff+6+2, client_slave_macaddr, 6);
-    Serial.printf("len = %d==%02x\r\n", len, len);
     buff[6+6+2] = len;
     memcpy(buff+6+2+6+1, data, len);
+    Serial.printf("espnow payload len = %d, hex = %02x\r\n", len, len);
 
     byte sum2 = 0;
     for (size_t i = 1; i <= len+2+6+6; i++) {
@@ -156,22 +154,22 @@ void setup() {
     }
     buff[len+2+6+6] = sum2;
 
-    Serial.println("===============");
-    Serial.printf("CHECK SUM 2 = %02x \r\n", sum2);
     for (size_t i = 1; i <= len+2+6+6+1; i++) {
       Serial.printf("%02x ", buff[i-1]);
-      if (i == 2 || i == 6+2 || i == 6+2+6) {
+      if (i == 2 || i == 6+2 || i == 6+2+6 || i == 6+2+6+1) {
         Serial.println();
       }
     }
 
-    Serial.println();
-    Serial.printf("free heap = %lu \r\n", ESP.getFreeHeap());
-
+    Serial.println("===============");
+    Serial.printf("checksum = %02x \r\n", sum2);
+    // len + start + mac1 + mac2 + sum
     swSerial.write(buff, len+2+6+6+1);
     swSerial.write('\r');
     swSerial.write('\n');
 
+    Serial.println();
+    Serial.printf("free heap = %lu \r\n", ESP.getFreeHeap());
   });
 
   esp_now_register_send_cb([](uint8_t* macaddr, uint8_t status) {
