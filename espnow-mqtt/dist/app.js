924b1827e45e956e7f8015ccfe9981102af7ae84
Nat <nat@cmmc.io>
2017-07-10 15:40:08 +0700

    the new version built

--- espnow-mqtt/dist/app.js
+++ espnow-mqtt/dist/app.js
@@ -5,11 +5,15 @@ var _conf = require('./conf');
 
 var _utils = require('./utils');
 
+var Message = _interopRequireWildcard(_utils);
+
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
+
 var chalk = require('chalk');
 var mqtt = require('mqtt');
 var client = mqtt.connect('mqtt://' + _conf.CONFIG.MQTT.HOST);
 var moment = require('moment-timezone');
-var _ = require('underscore');
+// const _ = require('underscore')
 
 client.on('connect', function () {
   console.log('mqtt connected being subscribed to ' + _conf.CONFIG.MQTT.SUB_TOPIC);
@@ -17,7 +21,6 @@ client.on('connect', function () {
 });
 
 client.on('packetsend', function (packet) {
-  // console.log(`packetsend`, packet);
   if (packet.cmd === 'publish') {
     console.log('published to ' + chalk.green(packet.topic));
   } else if (packet.cmd === 'subscribe') {
@@ -26,80 +29,46 @@ client.on('packetsend', function (packet) {
 });
 
 client.on('message', function (topic, message) {
-  console.log('==================================');
-  // console.log(`orig message = `, message);
-
-  // rhythm 0xd0xa$
-  if (message[message.length - 2] === 0x0d) {
-    message = message.slice(0, message.length - 2);
-  }
-  // console.log(`     message = `, message);
-
-  var statusObject = {};
-  if ((0, _utils.checksum)(message)) {
-    if (message[0] === 0xfc && message[1] === 0xfd) {
-      var len = message[2 + 6 + 6];
-      var mac1 = message.slice(2, 2 + 6);
-      var mac2 = message.slice(2 + 6, 2 + 6 + 6);
-      var payload = message.slice(2 + 6 + 6 + 1, message.length - 1);
-
-      if (payload[0] === 0xff && payload[1] === 0xfa) {
-        var _payload = payload.slice(2).toString('hex');
-        console.log('len = ' + chalk.cyan(len) + ', payload = ' + chalk.yellow('fffa') + chalk.cyan(_payload));
-        var type = payload.slice(2, 5);
-        var name = payload.slice(5, 11);
-        var mac1String = mac1.toString('hex');
-        var mac2String = mac2.toString('hex');
-        var val1 = payload.readUInt32LE(11) || 0,
-            val2 = payload.readUInt32LE(15) || 0,
-            val3 = payload.readUInt32LE(19) || 0,
-            batt = payload.readUInt32LE(23) || 0;
-
-
-        _.extend(statusObject, {
-          myName: name.toString(),
-          type: type.toString('hex'),
-          sensor: type.toString('hex'),
-          val1: parseInt(val1.toString()),
-          val2: parseInt(val2.toString()),
-          val3: parseInt(val3.toString()),
-          batt: parseInt(batt.toString()),
-          mac1: mac1String,
-          mac2: mac2String,
-          updated: moment().unix().toString(),
-          updatedText: moment().tz('Asia/Bangkok').format('DD/MMMM/YYYY, hh:mm:ss a')
-        });
-
-        console.log('==================================');
-        console.log('type = ', statusObject.type);
-        console.log('name = ', statusObject.name);
-        console.log('val1 = ', statusObject.val1);
-        console.log('val2 = ', statusObject.val2);
-        console.log('val3 = ', statusObject.val3);
-        console.log('batt = ', statusObject.batt);
-        console.log('[master] mac1 = ', statusObject.mac1);
-        console.log('[ slave] mac2 = ', statusObject.mac2);
-        console.log('==================================');
-
-        var serializedObjectJsonString = JSON.stringify(statusObject);
+  if (Message.isValidInComingMessage(message)) {
+    var payload = Message.getPayloadByStrip0D0A(message);
+    if (payload !== null) {
+      var parsedResult = Message.parsePayload(payload);
+      var pubTopics = [];
+      var mac1 = parsedResult.mac1;
+      var mac2 = parsedResult.mac2;
+      if (parsedResult.payloadType === Message.Constants.PAYLOAD_FCFD_TYPE_DATA) {
+        var parsedData = Message.parseDataPayload(parsedResult.data, Message.getPayloadType(message));
+        delete parsedResult.payloadType;
+        console.log(' parsedData = ', parsedData);
+        console.log(' parsedResult = ', parsedResult);
+        // console.log(`${type} ${name} ${val1} ${val2} ${val3} ${batt}`)
+        var serializedObjectJsonString = JSON.stringify(parsedData);
+        // `${CONFIG.MQTT.PUB_PREFIX}/raw/${mac1String}/${mac2String}/status`,
         // eslint-disable-next-line no-unused-vars
-        var pubTopics = [_conf.CONFIG.MQTT.PUB_PREFIX + '/' + mac1String + '/' + mac2String + '/status', _conf.CONFIG.MQTT.PUB_PREFIX + '/raw/' + mac1String + '/' + mac2String + '/status', _conf.CONFIG.MQTT.PUB_PREFIX + '/' + mac1String + '/' + name.toString() + '/status'].forEach(function (topic, idx) {
+        pubTopics = [_conf.CONFIG.MQTT.PUB_PREFIX + '/' + mac1 + '/' + mac2 + '/status', _conf.CONFIG.MQTT.PUB_PREFIX + '/' + mac1 + '/' + parsedData.name + '/status'];
+        pubTopics.forEach(function (topic, idx) {
           client.publish(topic, serializedObjectJsonString, { retain: false });
         });
-      } else {
-        console.log('invalid header');
+        console.log(pubTopics);
+      } else if (parsedResult.payloadType === Message.Constants.PAYLOAD_FAFB_TYPE_DEVICE_REGISTRATION) {
+        delete parsedResult.payloadType;
+        parsedResult.time = new Date().getTime();
+        var serializedDevice = JSON.stringify(parsedResult);
+        pubTopics = [_conf.CONFIG.MQTT.PUB_PREFIX + '/' + mac1 + '/command'];
+        pubTopics.forEach(function (topic, idx) {
+          client.publish(topic, serializedDevice, { retain: false });
+        });
       }
+    } else {
+      console.log(message);
+      console.log('================');
+      console.log('================');
+      console.log(message.length);
+      console.log('invalid packet header.');
+      console.log('================');
+      console.log('================');
     }
-  } else {
-    console.log(message);
-    console.log('================');
-    console.log('================');
-    console.log(message.length);
-    console.log('invalid checksum');
-    console.log('================');
-    console.log('================');
   }
 });
-
 console.log('Application started ' + moment().tz('Asia/Bangkok'));
 //# sourceMappingURL=app.js.map
--- espnow-mqtt/dist/utils.js
+++ espnow-mqtt/dist/utils.js
@@ -6,20 +6,140 @@ Object.defineProperty(exports, "__esModule", {
 /** * Created by nat on 7/5/2017 AD.
  */
 
-var chalk = require('chalk');
+var slice = exports.slice = function slice(arr, idx, len) {
+  return arr.slice(idx, idx + len);
+};
 
-var checksum = exports.checksum = function checksum(message) {
+var calculateChecksum = exports.calculateChecksum = function calculateChecksum(message) {
   var calculatedSum = 0;
-  var checkSum = message[message.length - 1];
-  for (var i = 0; i < message.length - 1; i++) {
+  var lastIdx = message.length;
+  for (var i = 0; i < lastIdx; i++) {
     calculatedSum ^= message[i];
   }
-  console.log('calculated sum = ' + chalk.yellow(hexChar(calculatedSum)));
-  console.log('    >check sum = ' + chalk.green(hexChar(checkSum)));
+  return calculatedSum;
+};
+
+var checksum = exports.checksum = function checksum(message) {
+  var checkSum = message[message.length - 1];
+  var data = slice(message, 0, message.length - 1);
+  var calculatedSum = calculateChecksum(data);
   return calculatedSum === checkSum;
+};
+
+/*
+ * valid is
+ *    startwith 0xfc, 0xfd
+ *    endwith 0x0d 0x0a
+ */
+var isValidInComingMessage = exports.isValidInComingMessage = function isValidInComingMessage(message) {
+  var msgLength = message.length;
+
+  var payloadType = getPayloadType(Buffer.from(message));
+  var isValidHeaderBytes = payloadType !== Constants.PAYLOAD_TYPE_UNKNOWN;
+  var isValidEndBytes = slice(message, msgLength - 2, 2).equals(Buffer.from([0x0d, 0x0a]));
+
+  return isValidHeaderBytes && isValidEndBytes;
+};
+
+var getPayloadByStrip0D0A = exports.getPayloadByStrip0D0A = function getPayloadByStrip0D0A(message) {
+  if (isValidInComingMessage(message)) {
+    return message.slice(message, message.length - 2);
+  } else {
+    return null;
+  }
+};
+
+var parsePayload = exports.parsePayload = function parsePayload(payload) {
+  // START BYTE = 2 BYTES
+  // MAC ADDR   = 6 BYTES
+  // DATA LEN   = 1 BYTES
+  var IDX = {
+    START_BYTES: 0,
+    MAC_1: 2,
+    MAC_2: 2 + 6,
+    DATA_PAYLOAD: 2 + 6 + 6 + 1
+  };
+
+  var payloadType = getPayloadType(payload);
+
+  if (payloadType === Constants.PAYLOAD_FAFB_TYPE_DEVICE_REGISTRATION) {
+    // const type = payload.slice(2, 6)
+    var mac1 = slice(payload, IDX.MAC_1 + 4, 6).toString('hex');
+    var mac2 = slice(payload, IDX.MAC_2 + 4, 6).toString('hex');
+    // mac1 = SOFT_AP_IF
+    // mac2 = STA_IF
+    return { mac1: mac1, mac2: mac2, payloadType: payloadType };
+  } else if (payloadType === Constants.PAYLOAD_FCFD_TYPE_DATA) {
+    var _mac = slice(payload, IDX.MAC_1, 6).toString('hex');
+    var _mac2 = slice(payload, IDX.MAC_2, 6).toString('hex');
+    var len = payload[2 + 6 + 6];
+    var dataPayload = slice(payload, IDX.DATA_PAYLOAD, len);
+    return { len: len, mac1: _mac, mac2: _mac2, data: dataPayload, payloadType: payloadType };
+  } else {
+    return {
+      payloadType: payloadType
+    };
+  }
+};
+
+var parseDataPayload = exports.parseDataPayload = function parseDataPayload(payload, parserType) {
+  // console.log(`parseDataPayload = `, typeof payload, payload, payload.toString('hex'))
+  if (parserType === Constants.PAYLOAD_FCFD_TYPE_DATA) {
+    var type = payload.slice(2, 6);
+    var _ref = [payload.readUInt32LE(6) || 0, payload.readUInt32LE(10) || 0, payload.readUInt32LE(14) || 0, payload.readUInt32LE(18) || 0, payload.readUInt8(22)],
+        val1 = _ref[0],
+        val2 = _ref[1],
+        val3 = _ref[2],
+        batt = _ref[3],
+        nameLen = _ref[4];
+
+
+    var name = slice(payload, 23, nameLen);
+    return {
+      type: type.toString('hex'),
+      val1: parseInt(val1.toString(), 10),
+      val2: parseInt(val2.toString(), 10),
+      val3: parseInt(val3.toString(), 10),
+      batt: parseInt(batt.toString(), 10),
+      name: name.toString()
+    };
+  } else if (parserType === Constants.PAYLOAD_FAFB_TYPE_DEVICE_REGISTRATION) {
+    // console.log(`parserType = DEVICE REGISTRATION`)
+  } else {
+      // console.log(`parserType = UNKNOWN`)
+    }
+};
+
+var getPayloadType = exports.getPayloadType = function getPayloadType(payload) {
+  var startBytes = slice(payload, 0, 2);
+  if (startBytes.equals(createBufferFromHexString('fafb'))) {
+    return Constants.PAYLOAD_FAFB_TYPE_DEVICE_REGISTRATION;
+  } else if (startBytes.equals(createBufferFromHexString('fcfd'))) {
+    return Constants.PAYLOAD_FCFD_TYPE_DATA;
+  } else {
+    return Constants.PAYLOAD_TYPE_UNKNOWN;
+  }
 };
 
 var hexChar = exports.hexChar = function hexChar(b) {
   return b.toString(16);
+};
+var hexFromChar = exports.hexFromChar = function hexFromChar(c) {
+  return c.charCodeAt(0);
+};
+var UInt32LEByte = exports.UInt32LEByte = function UInt32LEByte(val) {
+  var buff = Buffer.allocUnsafe(4);
+  buff.writeUInt32LE(val, 0);
+  return buff;
+};
+
+var createBufferFromHexString = exports.createBufferFromHexString = function createBufferFromHexString(input) {
+  return Buffer.from(input, 'hex');
+};
+
+var Constants = exports.Constants = {
+  PAYLOAD_FAFB_TYPE_DEVICE_REGISTRATION: 'PAYLOAD_FAFB_TYPE_DEVICE_REGISTRATION',
+  PAYLOAD_FCFD_TYPE_DATA: 'PAYLOAD_FCFD_TYPE_DATA',
+  PAYLOAD_TYPE_UNKNOWN: 'PAYLOAD_TYPE_UNKNOWN'
 };
 //# sourceMappingURL=utils.js.map
