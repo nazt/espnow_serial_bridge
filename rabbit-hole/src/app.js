2728ea31a9bb9dc2b162e07cedf8df96f577ca03
Nat <nat@cmmc.io>
2017-07-13 01:09:58 +0700

    add tcp server

--- rabbit-hole/src/app.js
+++ rabbit-hole/src/app.js
@@ -1,5 +1,7 @@
 #!/bin/env node
 
+import net from 'net'
+import pkg from '../package.json'
 import { CONFIG } from './conf'
 
 const chalk = require('chalk')
@@ -7,3 +9,15 @@ const moment = require('moment-timezone')
 
 console.log(chalk, CONFIG)
 console.log(`Application started ${moment().tz('Asia/Bangkok')}`)
+
+const PORT = process.env.PORT || 10777
+console.log(`starting application v.${pkg.version} on port ${PORT} `)
+
+const server = net.createServer((socket) => {
+  socket.name = `${socket.remoteAddress}:${socket.remotePort}`
+  // incoming data
+  socket.on('data', (data) => {
+
+  })
+})
+server.listen(PORT, '0.0.0.0')
