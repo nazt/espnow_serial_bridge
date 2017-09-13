3d4e3526ae86395f3c475fef8b9c71307105a454
Nat <nat@cmmc.io>
2017-09-13 11:28:12 +0700

    remove unused.

--- espnow-controller/espnow-controller/espnow-controller.ino
+++ espnow-controller/espnow-controller/espnow-controller.ino
@@ -1,5 +1,4 @@
 #include <Arduino.h>
-#include <SoftwareSerial.h>
 #include <ESP8266WiFi.h>
 #include <Ticker.h>
 #include "datatypes.h"
@@ -7,96 +6,73 @@ extern "C" {
   #include <espnow.h>
   #include <user_interface.h>
 }
-#define rxPin 14
-#define txPin 12
-
-SoftwareSerial swSerial(rxPin, txPin);
-
-#define WIFI_DEFAULT_CHANNEL 1
-#define CMMC_DEBUG_SERIAL 1
-#if CMMC_DEBUG_SERIAL
-    #define CMMC_DEBUG_PRINTER Serial
-    #define CMMC_DEBUG_PRINT(...) { CMMC_DEBUG_PRINTER.print(__VA_ARGS__); }
-    #define CMMC_DEBUG_PRINTLN(...) { CMMC_DEBUG_PRINTER.println(__VA_ARGS__); }
-    #define CMMC_DEBUG_PRINTF(...) { CMMC_DEBUG_PRINTER.printf(__VA_ARGS__); }
-#else
-    #define CMMC_DEBUG_PRINT(...) { }
-    #define CMMC_DEBUG_PRINTLN(...) { }
-    #define CMMC_DEBUG_PRINTF(...) { }
-#endif
-
 bool ledState = LOW;
 Ticker ticker;
 uint8_t self_ap_slave_macaddr[6];
 uint8_t self_sta_master_macaddr[6];
 
-uint8_t buff[50];
+#define BUTTON_PIN 0
+
+// uint8_t buff[50];
 
 // SOFTAP_IF
 void printMacAddress(uint8_t* macaddr) {
-  // CMMC_DEBUG_PRINT("{");
-  // for (int i = 0; i < 6; i++) {
-  //   CMMC_DEBUG_PRINT("0x");
-  //   CMMC_DEBUG_PRINT(macaddr[i], HEX);
-  //   if (i < 5) CMMC_DEBUG_PRINT(',');
-  // }
-  // CMMC_DEBUG_PRINTLN("};");
+  Serial.print("{");
+  for (int i = 0; i < 6; i++) {
+    Serial.print("0x");
+    Serial.print(macaddr[i], HEX);
+    if (i < 5) Serial.print(',');
+  }
+  Serial.print("};");
 }
 
 uint32_t freqCounter = 0;;
 
 void setup() {
-  #if CMMC_DEBUG_SERIAL
-    Serial.begin(230400);
-    delay(10);
-    Serial.flush();
-  #endif
-
-  ticker.attach_ms(1000, []() {
-    // Serial.printf("%d/s\r\n", freqCounter);
-    freqCounter = 0;
-  });
-
+  Serial.begin(230400);
+  delay(10);
+  Serial.flush();
+  Serial.println("after ticker");
   WiFi.disconnect();
   pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
 
-  pinMode(rxPin, INPUT);
-  pinMode(txPin, OUTPUT);
-  swSerial.begin(9600);
-
-  CMMC_DEBUG_PRINTLN("Initializing... Controller..");
+  Serial.print("Initializing... Controller..");
   WiFi.mode(WIFI_STA);
   uint8_t macaddr[6];
 
   wifi_get_macaddr(STATION_IF, macaddr);
-  CMMC_DEBUG_PRINT("[master] address (STATION_IF): ");
-  memcpy(self_sta_master_macaddr, macaddr, 6);
+  Serial.print("[master] address (STATION_IF): ");
   printMacAddress(macaddr);
+  Serial.println();
 
   wifi_get_macaddr(SOFTAP_IF, macaddr);
-  CMMC_DEBUG_PRINT("[slave] address (SOFTAP_IF): ");
+  Serial.print("[slave] address (SOFTAP_IF): ");
   printMacAddress(macaddr);
-  memcpy(self_ap_slave_macaddr, macaddr, 6);
+  Serial.println();
+  // memcpy(self_sta_master_macaddr, macaddr, 6);
+  // memcpy(self_ap_slave_macaddr, macaddr, 6);
+
+  ticker.attach_ms(1000, []() {
+    Serial.printf("%d/s\r\n", freqCounter);
+    freqCounter = 0;
+  });
 
   if (esp_now_init() == 0) {
-    CMMC_DEBUG_PRINTLN("direct link  init ok");
+    Serial.print("direct link  init ok");
   } else {
-    CMMC_DEBUG_PRINTLN("dl init failed");
+    Serial.print("dl init failed");
     ESP.restart();
     return;
   }
 
-  // swSerial.println("ESPNOW SEND HELLO");
-  // swSerial.write("HELLO", 5);
-
-  bzero(buff, sizeof(buff));
+  // // swSerial.println("ESPNOW SEND HELLO");
+  // // swSerial.write("HELLO", 5);
+  //
   esp_now_set_self_role(ESP_NOW_ROLE_CONTROLLER);
   esp_now_register_recv_cb([](uint8_t *macaddr, uint8_t *data, uint8_t len) {
     freqCounter++;
     // Serial.println("RECEIVE... ");
-    PACKET_T pkt;
-    memcpy(&pkt, data, sizeof(pkt));
-    SENSOR_T sensor_data = pkt.data;
 
     // for (size_t i = 0; i < len; i++) {
     //   // Serial.print(data[i], HEX);
@@ -115,28 +91,28 @@ void setup() {
     // Serial.printf("myName= %s\r\n", sensor_data.myName);
 
     // printMacAddress(macaddr);
+    if (digitalRead(BUTTON_PIN) == LOW) {
+      // PACKET_T pkt;
+      // memcpy(&pkt, data, sizeof(pkt));
+      // SENSOR_T sensor_data = pkt.data;
+      Serial.write(data, len);
+    }
+
     digitalWrite(LED_BUILTIN, ledState);
     ledState = !ledState;
-    Serial.write(data, len);
-    //
-    // uint8_t *client_slave_macaddr = macaddr;
-    // Serial.println();
-    // swSerial.write('\r');
-    // swSerial.write('\n');
-    // Serial.write(data, len);
   });
-
+  //
   esp_now_register_send_cb([](uint8_t* macaddr, uint8_t status) {
-    // CMMC_DEBUG_PRINT("send_cb to ");
+    // Serial.print("send_cb to ");
     printMacAddress(macaddr);
     static uint32_t ok = 0;
     static uint32_t fail = 0;
     if (status == 0) {
-      CMMC_DEBUG_PRINTLN("ESPNOW: SEND_OK");
+      Serial.print("ESPNOW: SEND_OK");
       ok++;
     }
     else {
-      CMMC_DEBUG_PRINTLN("ESPNOW: SEND_FAILED");
+      Serial.print("ESPNOW: SEND_FAILED");
       fail++;
     }
     // Serial.printf("[SUCCESS] = %lu/%lu \r\n", ok, ok + fail);
